458fbfdd07817c42078fd664ccdbede444a271f0
Arktetra <sameershrestha.sm@gmail.com>
2024-10-27 21:18:28 +0545

    add AyeModule

---
+++ aye/core/module.py
@@ -0,0 +1,42 @@
+"""The AyeModule - an nn.Module with additional features."""
+
+from typing import (
+    Any, 
+    Callback, 
+    Sequence, 
+    Union
+)
+from typing_extensions import override
+
+import torch
+import torch.nn as nn
+
+class AyeModule(nn.Module):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__()
+        
+    @override
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
+        return super().forward(*args, **kwargs)
+        
+    def training_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+        raise NotImplementedError("`training_step` must be implemented to be used with the Aye Learner.")
+    
+    def validation_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+        pass
+        
+    def test_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+        pass 
+    
+    def predict_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+        batch = kwargs.get("batch", args[0])
+        return self(batch)
+    
+    def configure_optimizers(self):
+        raise NotImplementedError("`configure_optimizer` must be implemented to be used with the Aye Learner.")
+    
+    def configure_callbacks(self) -> Union[Sequence[Callback], Callback]:
+        return []
+    
+    def backward(self, loss: torch.Tensor, *args: Any, **kwargs: Any) -> None:
+        loss.backward(*args, **kwargs)
