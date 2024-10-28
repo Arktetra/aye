d7c0a7a9dd1352d0580f991eaa0306dc34a4a933
Arktetra <sameershrestha.sm@gmail.com>
2024-10-28 21:02:38 +0545

    update learner to use callbacks

--- aye/learner/learner.py
+++ aye/learner/learner.py
@@ -1,6 +1,7 @@
 from aye import AyeModule
+from aye.callbacks import Callback, with_callbacks, run_callbacks
 from torch.utils.data import DataLoader
-from typing import Optional
+from typing import Optional, Sequence, Tuple
 
 import torch
 
@@ -8,41 +9,76 @@ TRAIN_DATALOADER = DataLoader
 VAL_DATALOADER = DataLoader
 
 class Learner:
-    def __init__(self, accelerator: str = None, epochs = 5) -> None:
+    def __init__(self, accelerator: str = None, epochs = 5, callbacks: Sequence[Callback] = None) -> None:
         super().__init__()
         self.accelerator = accelerator
         self.epochs = epochs
+        self.callbacks = callbacks
+        self.log_dict = {}
         
+    def log(self):
+        print(self.log_dict)
+
+    @with_callbacks("batch")
+    def fit_one_batch(
+        self, 
+        model: AyeModule, 
+        batch: Tuple[torch.Tensor, torch.Tensor], 
+        batch_idx: int, 
+        optimizer: torch.optim.Optimizer, 
+        training: bool
+    ):
+        batch = batch[0].to("cuda"), batch[1].to("cuda")
+                    
+        if training:
+            loss = model.training_step(batch, batch_idx)
+            model.backward(loss)
+            model.optimizer_step(optimizer)
+            model.optimizer_zero_grad(optimizer)
+        else:
+            loss = model.validation_step(batch, batch_idx)
+                        
+        self.loss += loss / len(batch)
+            
+    @with_callbacks("epoch")
+    def fit_epoch(
+        self, 
+        model: AyeModule, 
+        train_dataloader: TRAIN_DATALOADER, 
+        val_dataloader: VAL_DATALOADER, 
+        optimizer: torch.optim.Optimizer, 
+        epoch: int
+    ):
+        self.loss = 0.
+        for batch_idx, batch in enumerate(train_dataloader):
+            self.fit_one_batch(model, batch, batch_idx, optimizer, training = True)
+            self.train_loss = self.loss / len(train_dataloader)
+            
+        self.loss = 0.
+        model.eval()
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(val_dataloader):
+                self.fit_one_batch(model, batch, batch_idx, optimizer, training = False)
+                self.val_loss = self.loss / len(val_dataloader)
+                
+        self.log_dict["epoch"] = epoch
+        self.log_dict["train_loss"] = self.train_loss.item()
+        self.log_dict["val_loss"] = self.val_loss.item()
+
+    @with_callbacks("fit")
     def fit(
         self,
         model: AyeModule,
         train_dataloader: Optional[TRAIN_DATALOADER] = None,
         val_dataloader: Optional[VAL_DATALOADER] = None,
-    ) -> None:
-        train_loss, val_loss = 0., 0.
-        
+    ) -> None:    
         if self.accelerator == "cuda":
             model.to("cuda")
         
         optimizer = model.configure_optimizers()
         
         for epoch in range(self.epochs):
-            for batch_idx, batch in enumerate(train_dataloader):
-                batch = batch[0].to("cuda"), batch[1].to("cuda")
-                loss = model.training_step(batch, batch_idx)
-                model.backward(loss)
-                model.optimizer_step(optimizer)
-                model.optimizer_zero_grad(optimizer)
-                train_loss += loss / len(batch)
-            train_loss /= len(train_dataloader)
-            
-            model.eval()
-            with torch.no_grad():
-                for batch_idx, batch in enumerate(val_dataloader):
-                    batch = batch[0].to("cuda"), batch[1].to("cuda")
-                    loss = model.validation_step(batch, batch_idx)
-                    val_loss += loss / len(batch)
-                val_loss /= len(val_dataloader)
-                
-            print(f"epoch: {epoch}, train_loss: {train_loss:.4f}, val_loss: {val_loss:.4f}")
-        
+            self.fit_epoch(model, train_dataloader, val_dataloader, optimizer, epoch)
+                                
+    def callback(self, method_name):
+        run_callbacks(self.callbacks, method_name, self)
