1d6b5e1d1288dc059d2d7089146b6016795a26d1
Arktetra <sameershrestha.sm@gmail.com>
2024-11-20 08:44:00 +0545

    change TimeMachine class to be more flexible

--- aye/data_modules/time_machine.py
+++ aye/data_modules/time_machine.py
@@ -12,13 +12,22 @@ TRAIN_SIZE = metadata.TRAIN_SIZE
 VAL_SIZE = metadata.VAL_SIZE
 
 class TimeMachine(DataModule):
-    def __init__(self, batch_size: int = None):
+    def __init__(
+        self, 
+        batch_size: int = None,
+        train_size: int = None, 
+        val_size: int = None
+    ):
         super().__init__()
         self.train_transform = None
         self.data_dir = metadata.DL_DATA_DIRNAME
         
         if batch_size:
             self.batch_size = batch_size
+            
+        self.train_size = train_size if train_size else TRAIN_SIZE
+        self.val_size = val_size if val_size else VAL_SIZE 
+        self.total_size = self.train_size + self.val_size
         
     def prepare_data(self):
         metadata = toml.load(METADATA_FILENAME)
@@ -33,6 +42,7 @@ class TimeMachine(DataModule):
             corpus, self.vocab = self.build()
             array = torch.tensor([corpus[i:i + metadata.TIME_STEPS + 1]
                                 for i in range(len(corpus) - metadata.TIME_STEPS)])
+            array = array[:self.total_size, :self.total_size]
             x, y = array[:, :-1], array[:, 1:]
             return BaseDataset(x, y, transform, target_transform)
         
@@ -41,7 +51,7 @@ class TimeMachine(DataModule):
         train_val_dataset = _load_dataset(transform = self.train_transform, target_transform = target_transform)
         self.train_dataset, self.val_dataset = split_dataset(
             train_val_dataset, 
-            fraction = metadata.TRAIN_SIZE / (metadata.TRAIN_SIZE + metadata.VAL_SIZE), 
+            fraction = self.train_size / self.total_size, 
             seed = 41
         )
         
