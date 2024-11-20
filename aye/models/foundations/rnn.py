f713ea5c6e497bff4b4fca9fa9b0ef0878746277
Arktetra <sameershrestha.sm@gmail.com>
2024-11-20 17:22:18 +0545

    add implementations for deep RNN

--- aye/models/foundations/rnn.py
+++ aye/models/foundations/rnn.py
@@ -38,14 +38,45 @@ class RNNScratch(nn.Module):
             
         return outputs, state
     
+class DeepRNNScratch(nn.Module):
+    def __init__(self, num_inputs, num_hiddens, num_layers, sigma = 0.01, device = None):
+        super().__init__()
+        self.num_inputs = num_inputs
+        self.num_hiddens = num_hiddens
+        self.num_layers = num_layers 
+        self.sigma = sigma
+        self.device = device 
+        
+        self.rnns = nn.Sequential(*[RNNScratch(num_inputs if i == 0 else num_hiddens, num_hiddens, sigma, device) 
+                                    for i in range(num_layers)]
+        )
+        
+    def forward(self, inputs, Hs = None):
+        outputs = inputs
+        
+        if Hs is None:
+            Hs = [None] * self.num_layers
+            
+        for i in range(self.num_layers):
+            outputs, Hs[i] = self.rnns[i](outputs, Hs[i])
+            outputs = torch.stack(outputs, dim = 0)
+            
+        return outputs, Hs
+        
+    
 class RNN(AyeModule):
     """RNN model implemented with high-level APIs."""
-    def __init__(self, num_inputs, num_hiddens, sigma = 0.01, device = None):
+    def __init__(self, num_inputs, num_hiddens, num_layers = 1, device = None):
         super().__init__()
         self.num_inputs = num_inputs 
         self.num_hiddens = num_hiddens 
+        self.num_layers = num_layers
         
-        self.rnn = nn.RNN(input_size = num_inputs, hidden_size = num_hiddens, device = device)
+        self.rnn = nn.RNN(
+            input_size = num_inputs, hidden_size = num_hiddens, 
+            num_layers = num_layers, device = device
+        )
         
     def forward(self, inputs, state = None):
         return self.rnn(inputs, state)
+        
