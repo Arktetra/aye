55f2b4ad49818e631ede7bb0321568d6aaea0df9
Arktetra <sameershrestha.sm@gmail.com>
2024-11-28 19:09:33 +0545

    add predict step for seq2seq model

--- aye/models/foundations/seq2seq.py
+++ aye/models/foundations/seq2seq.py
@@ -1,4 +1,5 @@
 from aye.models.foundations.encoder_decoder import Encoder, Decoder, EncoderDecoder
+from aye.models.init import init_seq2seq_weights
 
 import torch
 import torch.nn as nn
@@ -12,6 +13,7 @@ class Seq2SeqEncoder(Encoder):
         embed_size: int, 
         num_hiddens: int,
         num_layers: int, 
+        dropout: float = 0.1,
         device = None
     ):
         super().__init__()
@@ -21,7 +23,12 @@ class Seq2SeqEncoder(Encoder):
         self.device = device
         
         self.embedding = nn.Embedding(num_embeddings = vocab_size, embedding_dim = embed_size)
-        self.rnn = nn.GRU(input_size = embed_size, hidden_size = num_hiddens, num_layers = num_layers)
+        self.rnn = nn.GRU(
+            input_size = embed_size, hidden_size = num_hiddens, 
+            num_layers = num_layers, dropout = dropout
+        )
+        
+        self.apply(init_seq2seq_weights)
         
     def forward(self, X: torch.Tensor, *args):              # X.shape = (batch_size, time_steps)
         embeds = self.embedding(X.t().type(torch.int64))    # embeds.shape = (time_steps, batch_size, embed_size)
@@ -36,6 +43,7 @@ class Seq2SeqDecoder(Decoder):
         embed_size: int,
         num_hiddens: int,
         num_layers: int,
+        dropout: float = 0.1,
         device = None
     ):
         super().__init__()
@@ -46,14 +54,19 @@ class Seq2SeqDecoder(Decoder):
         self.device = device 
         
         self.embedding = nn.Embedding(num_embeddings = vocab_size, embedding_dim = embed_size)
-        self.rnn = nn.GRU(input_size = embed_size + num_hiddens, hidden_size = num_hiddens, num_layers = num_layers)
+        self.rnn = nn.GRU(
+            input_size = embed_size + num_hiddens, hidden_size = num_hiddens, 
+            num_layers = num_layers, dropout = dropout
+        )
         self.dense = nn.LazyLinear(vocab_size)
+        
+        self.apply(init_seq2seq_weights)
         
     def init_state(self, enc_all_outputs, *args):
         return enc_all_outputs
         
     def forward(self, X: torch.Tensor, state):
-        embeds = self.embedding(X.t().type(torch.int64))
+        embeds = self.embedding(X.t().type(torch.int32))
         enc_output, hidden_state = state 
         
         context = enc_output[-1]
@@ -82,7 +95,6 @@ class Seq2Seq(EncoderDecoder):
     
     def __shared_step(self, batch, batch_idx):
         self.logits = self(*batch)
-        # return self.criterion(self.logits.swapdims(1, 2), batch[-1])
         return self.loss(self.logits.swapdims(1, 2), batch[-1])
     
     def training_step(self, batch, batch_idx):
@@ -99,3 +111,23 @@ class Seq2Seq(EncoderDecoder):
         l = self.criterion(Y_hat, Y, reduction = "none")
         mask = (Y != self.tgt_pad).type(torch.float32)
         return (l * mask).sum() / mask.sum()
+    
+    def predict_step(self, batch, device, num_steps, save_attention_weights = False):
+        batch = [b.to(device) for b in batch]
+        src, tgt, src_valid_len, _ = batch 
+        
+        enc_all_outputs = self.encoder(src, src_valid_len)
+        dec_state = self.decoder.init_state(enc_all_outputs, src_valid_len)
+                
+        outputs, attention_weights = [tgt[:, (0)].unsqueeze(1), ], []
+    
+        for _ in range(num_steps):
+            Y, dec_state = self.decoder(outputs[-1], dec_state)
+            outputs.append(Y.argmax(dim = 2))
+            
+            if save_attention_weights:
+                attention_weights.append(self.decoder.attention_weights)
+                
+        return torch.cat(outputs[:], dim = 1), attention_weights
+            
+        
